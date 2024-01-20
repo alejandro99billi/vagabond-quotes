ac752c0b40e69e4a87dc0407fbac30c0a8c4b041
alejandro99billi <96229780+alejandro99billi@users.noreply.github.com>
2024-01-20 20:33:45 +0100

    add firts quote

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit  {
   title = 'MusayiMiyamatoQuotes';
+  audioFiles = [
+    { title: 'Ladyfingers', link: '/assets/music/Ladyfingers.mp3' }
+  ]; 
+
+  ngOnInit() {
+    this.initAudio();
+  }
+ 
+  playMusic(){
+    const audio = document.getElementById('myAudio') as HTMLAudioElement;
+    audio.play();
+  }
+
+  autoplay = true;
+  initAudio() {
+    const audio = document.getElementById('myAudio') as HTMLAudioElement;
+    audio.play();
+  }
 }
